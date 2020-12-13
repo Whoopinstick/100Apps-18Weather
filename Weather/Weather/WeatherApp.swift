b5420d34f24741fc71c9d1b918c2f9e63e516bca
Whoopinstick <k.m.paul68@gmail.com>
2020-12-13 10:07:26 -0500

    initial project creation

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Kevin Paul on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
