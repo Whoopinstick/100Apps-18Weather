7dbe3e3bbe989837f1df5c367a7ad07748db3acf
Whoopinstick <k.m.paul68@gmail.com>
2020-12-27 15:58:33 -0500

    add navigation view, title, and bar buttons for settings and refresh

--- Weather/Weather/Screen/MainView.swift
+++ Weather/Weather/Screen/MainView.swift
@@ -10,34 +10,52 @@ import SwiftUI
 struct MainView: View {
     @ObservedObject var viewModel = MainViewViewModel()
     var body: some View {
-        ZStack {
-            VStack {
-                Text("\(viewModel.weather.lat)")
-                Text("\(viewModel.weather.lon)")
-                Text(viewModel.weather.timezone)
-                Text("\(viewModel.weather.current.temp, specifier: "%.0f")")
-                
-                Text("Daily - ")
-                ForEach(viewModel.weather.daily) { daily in
-                    HStack {
-                        Text("min: \(daily.temp.min, specifier: "%.0f")")
-                        Text("max: \(daily.temp.max, specifier: "%.0f")")
-                        Text("weather: \(daily.weatherID)")
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("\(viewModel.weather.lat)")
+                    Text("\(viewModel.weather.lon)")
+                    Text(viewModel.weather.timezone)
+                    Text("\(viewModel.weather.current.temp, specifier: "%.0f")")
+                    
+                    Text("Daily - ")
+                    ForEach(viewModel.weather.daily) { daily in
+                        HStack {
+                            Text("min: \(daily.temp.min, specifier: "%.0f")")
+                            Text("max: \(daily.temp.max, specifier: "%.0f")")
+                            Text("weather: \(daily.weatherID)")
+                        }
                     }
                 }
                 
+                if viewModel.isLoading {
+                    WWProgressView()
+                }
+                
+            }
+            .onAppear {
+                viewModel.getWeather()
             }
             
+            .navigationTitle("Weather")
+            .navigationBarItems(leading:
+                                    Button(action: {
+                                        
+                                    }) {
+                                        Image(systemName: "gearshape")
+                                            .renderingMode(.original)
+                                    },
+                                trailing:
+                                    Button(action: {
+                                        print("refreshing")
+                                        viewModel.getWeather()
+                                    }) {
+                                        Image(systemName: "arrow.clockwise")
+                                            .renderingMode(.original)
+                                    }
+            )
             
-            if viewModel.isLoading {
-                WWProgressView()
-            }
         }
-        .onAppear {
-            viewModel.getWeather()
-        }
-        
-        
     }
 }
 
